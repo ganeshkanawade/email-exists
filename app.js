1f4a187204d0fa435bea1151ce24f2bd13ea9423
Ganesh Kanawade <ganesh.kanawade@enyotalearning.com>
2017-12-19 14:39:26 +0530

    Check emails from excel file

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var parseXlsx = require('excel');
+var formidable = require('formidable');
+var util = require('util');
+var fs = require('fs');
+const { URL } = require('url');
+var emailExistence = require('email-existence');
 
 var app = express();
 
@@ -21,8 +27,59 @@ app.get('/', function(req, res){
 });
 
 app.post('/upload', function(req,res){
-    res.send('Uploading....');
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var oldpath = files.fileUpload.path;
+        var newpath =  path.join(__dirname,"files/" + files.fileUpload.name);        
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            parseXlsx(newpath, function(err, data) {
+                if(err) throw err;
+                // data is an array of arrays
+                var i = 0;
+                data.forEach(row => {
+                    if(i!=0)
+                    {
+                        var firstName = row[2].toLowerCase();
+                        var lastName = row[3].toLowerCase();                        
+                        var domain = getHostName(row[1]);
+                        var emailCombinations = [
+                            firstName+ '@'+ domain,
+                            lastName+ '@'+  domain,
+                            firstName+ '.' +lastName+ '@'+  domain,
+                            lastName+ '.' +firstName+ '@'+  domain
+                        ]
+                        
+                        
+                        emailCombinations.forEach(email=> {
+                            
+                            emailExistence.check(email, function(err,res){
+                                if(res)
+                                {
+                                    console.log(email);
+                                }
+                            });
+                        });
+                        
+                    }
+                    i += 1;
+                });
+            });
+
+        });
+    });
+    res.send('Done!!!!');
 });
+
+function getHostName(url) {
+    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+    return match[2];
+    }
+    else {
+        return null;
+    }
+}
 
 app.listen(8080, function(){
     
